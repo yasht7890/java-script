c814cd2e10861c2635b35bb7997b8b197468156d
yasht7890 <96128020+yasht7890@users.noreply.github.com>
2024-08-29 02:51:27 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 122441
+let accountEmail = "yash@gmail.com"
+var accountPassword = "123456"
+accountCity = "Ghaziabad"
+let accountState;
+
+// accountId = 2 // not allowed
+
+accountEmail = "hdgss@hc.com"
+accountPassword = "21213212121"
+accountCity = "Bengaluru"
+
+console.log(accountId)
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,35 @@
+let naam = "Yash"
+let age1= 20
+
+console.log(typeof  "yash")
+console.log(typeof age1 )
+
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3) // we are using nodejs, not browser
+
+console.log(3 
+    +
+     3) // code readability should be high
+
+console.log("Yash")
+
+
+let name = "Yash"
+let age = 21
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
