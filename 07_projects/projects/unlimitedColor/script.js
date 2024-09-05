43fbc8d123f09b382c5894cd167edb9d9f8e1b36
Yash Tyagi <96128020+yasht7890@users.noreply.github.com>
2024-09-05 15:31:35 +0000

    project

---
+++ 07_projects/projects/unlimitedColor/script.js
@@ -0,0 +1,29 @@
+// generte a random color
+
+const randomColor = function(){
+    const hex = "0123456789ABCDEF"
+    let color = "#"
+    for(let i = 0 ; i<6;i++){
+    color += hex[Math.floor(Math.random()*16 )]
+    }
+return color
+};
+
+const start = document.querySelector("#start")
+const stop = document.querySelector("#stop")
+let intervalId;
+const startChangingColor= function(){
+    if(!intervalId){
+    intervalId = setInterval(changeBgColor , 1000);
+    }
+    function changeBgColor(){
+        document.body.style.backgroundColor = randomColor();
+    }
+}
+const stopChangingColor= function(){
+    clearInterval(intervalId);
+    intervalId = null;
+}
+
+start.addEventListener("click", startChangingColor)
+stop.addEventListener("click", stopChangingColor)
